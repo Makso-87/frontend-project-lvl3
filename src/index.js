741f481455ab078ee80f5166dfde7c9c91ea7dcd
Maksim Popov <makso16108@gmail.com>
2021-05-28 17:12:58 +0300

    Added init.js and ability to initializing of app by init()

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import '@popperjs/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './styles/main.scss';
-import app from './modules/app';
+import init from './init';
 
-app();
+init();
